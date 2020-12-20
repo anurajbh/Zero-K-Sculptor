b938bf0a304722e522607519865e831c5e05cf24
anurajbh <anurajbh@users.noreply.github.com>
2020-12-20 15:45:17 -0500

    FPS controller, moved .gitignore to right location

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public float speed = 12f;
+
+    public bool isSprinting = false;
+
+    public float sprintBoost = 1.5f;
+
+    public float grav = -9.81f;
+
+    public bool isGrounded = true;
+
+    public GameObject groundCheck;
+
+    public LayerMask groundMask;
+
+    public float groundDistance = 0.4f;
+
+    public float jumpHeight = 40f;
+
+    Vector3 groundVelocity;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        groundCheck = GameObject.Find("GroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfSprint();
+        MovePlayer();
+    }
+    private void CheckIfSprint()
+    {
+        if(Input.GetAxisRaw("Fire3")!=0)
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+    }
+    private void MovePlayer()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundDistance, groundMask);
+        if(isGrounded && groundVelocity.y <0)
+        {
+            groundVelocity.y = -2f;
+        }
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        if (!isSprinting)
+        {
+            characterController.Move(move * speed * Time.deltaTime);
+        }
+        else
+        {
+            characterController.Move(move * speed * Time.deltaTime * sprintBoost);
+        }
+        if(Input.GetAxisRaw("Jump")!=0 && isGrounded)
+        {
+            groundVelocity.y = Mathf.Sqrt(-2f*grav * jumpHeight);
+        }
+        groundVelocity.y += grav * Time.deltaTime;
+        characterController.Move(groundVelocity * Time.deltaTime);
+    }
+}
