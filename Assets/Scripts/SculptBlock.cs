03e08bdca93b1e3cfce4e2e180d3d0a6ee4ad935
yuupsup <62122888+yuupsup@users.noreply.github.com>
2020-12-20 20:10:55 -0600

    Shard click order logic added
    Added logic to click shards in assigned order.
    When the shard is clicked that is correct in the pattern, it will be destroyed, otherwise, nothing will happen (currently).

--- Assets/Scripts/SculptBlock.cs
+++ Assets/Scripts/SculptBlock.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class SculptBlock : MonoBehaviour
 {
+    // Denotes the current index in the pattern.
+    // The value will be incremented when the player successfully clicks
+    // on the shard mesh in the pattern.
+    private int currentIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            child.tag = "Sculptable";
+            child.AddComponent<MeshCollider>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public bool isNextInPattern(GameObject hitObject)
+    {
+        Shard shard = hitObject.GetComponent<Shard>();
+        Debug.Log(shard.id + " " + currentIndex);
+        return shard.id == currentIndex;
+    }
+
+    public void Sculpt(GameObject hitObject)
+    {
+        Destroy(hitObject);
+        currentIndex++;
     }
 }
